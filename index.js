4cc926e0ae7ff3241cd062ee00d5bb82d31297ea
Derek White <derekwhite.m@gmail.com>
2023-02-02 20:00:17 -0500

    commit

---
+++ index.js
@@ -0,0 +1,28 @@
+function mergeSort(arr) {
+	if (arr.length < 2) return arr;
+	else {
+		const halfLength = Math.ceil(arr.length / 2);
+		const leftHalf = mergeSort(arr.slice(0, halfLength));
+		const rightHalf = mergeSort(arr.slice(halfLength));
+		return mergeHalves(leftHalf, rightHalf);
+	}
+}
+
+function mergeHalves(leftHalf, rightHalf) {
+	const mergedArr = [];
+	let iL = 0;
+	let iR = 0;
+	for (let i = 0; i < leftHalf.length + rightHalf.length; i++) {
+		const leftValue = leftHalf[iL];
+		const rightValue = rightHalf[iR];
+		if (leftValue < rightValue || rightValue === undefined) {
+			mergedArr.push(leftValue);
+			leftIndex++;
+		} 
+		else if (rightValue < leftValue || leftValue === undefined) {
+			mergedArr.push(rightValue);
+			rightIndex++;
+		}
+	}
+	return mergedArr;
+}
